6b33b7c72a42173506f63ec3e799bb795324c81a
kelseyberta <ber20002@byui.edu>
2023-01-12 17:39:16 -0500

    chamber

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -1,25 +1,8 @@
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
+const navbutton = document.querySelector("#nav-button");
+const navitems = document.querySelectorAll(".nav-item");
+// menuitems will be a Node List of the list items
 
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-  }
-}
-
-hamburger.addEventListener("click", toggleMenu);
-
-menuItems.forEach( 
-  function(menuItem) { 
-    menuItem.addEventListener("click", toggleMenu);
-  }
-)
+navbutton.addEventListener("click", () => {
+	navitems.forEach((item) => item.classList.toggle("open"));
+	navbutton.classList.toggle("close");
+});
