af9aae6f0d37ac4f2c3f753870371e14caa7fdc7
kelseyberta <ber20002@byui.edu>
2023-02-01 17:40:58 -0500

    test

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,54 +1,44 @@
-const requestURL = '/members/members.json';
+const url = 'https://kelseyberta.github.io/wdd230/chamber/members/members.json';
+
 const cards = document.querySelector('.cards');
 
+async function GetMemberData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.members);
+    displayMembers(data.members);
+}
+GetMemberData();
 
-fetch(requestURL)
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let icon = document.createElement('img');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('a');
+        
 
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    const businesses = jsonObject['businesses'];
-    businesses.forEach(displayBusinesses);
-  });
+        fullName.textContent = `${member.name}`;
+        address.textContent = `${member.address}`;
+        phone.textContent = `${member.phone}`;
+        website.innerHTML = `${member.url}`;
+        website.setAttribute('href', `${member.url}`);
+        
+        icon.setAttribute('src', member.icon);
+        icon.setAttribute('alt', `${member.name}`);
+        icon.setAttribute('loading', 'lazy');
+        icon.setAttribute('width', '200');
+        icon.setAttribute('height', '100');
 
+        card.appendChild(icon);
+        card.appendChild(fullName);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        
 
-
-  function displayBusinesses(business) {
-
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let image = document.createElement('img');
-    let infodiv = document.createElement('div')
-    let address = document.createElement('p');
-    let phone = document.createElement('p');
-    let url = document.createElement('p');
-    let level = document.createElement('p');
-
-  
-    card.setAttribute('class', 'busCard');
-    h2.textContent = `${business.name}`;
-    infodiv.setAttribute('class', 'infodiv')
-
-    address.innerHTML = `<strong>Address</strong>: ${business.address}`;
-    phone.innerHTML = `<strong>Phone #</strong>: ${business.phoneNumber}`;
-    url.innerHTML = `<strong>Website</strong>: <a href="http://${business.url}"> ${business.url}</a>`;
-    level.innerHTML = `<strong>Membership Level</strong>: ${business.membershipLevel}`;
-
-  
-
-    image.setAttribute('src', business.image);
-    image.setAttribute('alt', `Logo for ${business.name}`);
-    image.setAttribute('loading', 'lazy');
-  
-    card.appendChild(h2);
-    card.appendChild(image);
-    card.appendChild(infodiv);
-    infodiv.appendChild(address);
-    infodiv.appendChild(phone);
-    infodiv.appendChild(url);
-    infodiv.appendChild(level);
-
-  
-    document.querySelector('div.cards').appendChild(card);
-  }
+        cards.appendChild(card);
+    });
+}
--- chamber/scripts/viewChange.js
+++ chamber/scripts/viewChange.js
@@ -1,13 +1,18 @@
-const gridbutton = document.querySelector("#viewGrid");
-const listbutton = document.querySelector("#viewList");
-const display = document.querySelector("article");
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".article");
+
+
 
 gridbutton.addEventListener("click", () => {
-    display.classList.add("grid")
-    display.classList.remove("list");
-})
+	
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
 
-listbutton.addEventListener("click", () => {
-    display.classList.add("list");
-    display.classList.remove("grid");
-})
+listbutton.addEventListener("click", showList); 
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
